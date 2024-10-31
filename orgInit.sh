328247fd729b9b10a031cba2a5a8e6136ed0fb40
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-31 16:26:06 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,3 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
-sfdx force:source:push
-sfdx force:org:open
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p vscode -e extensions.demo
+
+sf project deploy start
+sf org open
